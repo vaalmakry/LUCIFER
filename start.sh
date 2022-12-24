8e73acc391f3b096008fde1972e86b3cd4fc7fa1
vaalmakry <120769934+vaalmakry@users.noreply.github.com>
2022-12-24 17:15:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-#if [ -z $UPSTREAM_REPO ]
-#then
-  #echo "Cloning main Repository"
-  #git clone https://github.com/No-OnE-Kn0wS-Me/FILTERMV24.git /Filtermv24
-#else
-  #echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  #git clone $UPSTREAM_REPO /Filtermv24
-#fi
-#cd /Filtermv24
-#pip3 install -U -r requirements.txt
-#echo "Starting Wednesday...."
-#python3 bot.py
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/vaalmakry/Lucifer.git /Lucifer
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Lucifer
+fi
+cd /Lucifer
+pip3 install -U -r requirements.txt
+echo "Starting Lucifer...."
+python3 bot.py
